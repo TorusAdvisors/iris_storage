4b3e5075fdd4b54aa22fc437ec60d6760bcd6066
Alexey Nechaev <nechaev.lexa@gmail.com>
2022-02-22 18:57:46 +0300

    fix

--- example_chunk_insert.js
+++ example_chunk_insert.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const storage_iris = require('@torusadvisors/iris_storage');
+const storage_iris = require('./lib/storage_iris');
 
 const IRIS_HOST = process.env.IRIS_HOST || '127.0.0.1';
 const IRIS_PORT = process.env.IRIS_PORT || 1972;
@@ -11,14 +11,12 @@ const IRIS_PWD = process.env.IRIS_PWD || 'SYS';
 const start_timer = Math.floor(+new Date() / 1000);
 
 let data = [];
-for (let i = 0; i < 30000; i++) {
+for (let i = 0; i < 100000; i++) {
     let k = 'key_'+i;
     let v = 'value_'+i;
     let item = {'item_key':k, 'item_value':v};
     data.push(item);
 }
-
-data = JSON.stringify(data);
 
 const storage = new storage_iris({host: IRIS_HOST, port: IRIS_PORT, ns: IRIS_NS, user: IRIS_USER, pwd: IRIS_PWD});
 
@@ -31,20 +29,20 @@ storage.processor.on('error', (err) => {
 storage.processor.on('ready', () => {
     console.log('start');
 
-    storage.dropIfExists('TORUS', 'Items')
+    storage.dropIfExists('TORUS', 'ITEMS')
         .then(() => {
-            console.log('Torus.Items dropped!');
+            console.log('TORUS.ITEMS dropped!');
 
-            let sql = "CREATE TABLE TORUS.Items (\n" +
+            let sql = "CREATE TABLE TORUS.ITEMS (\n" +
                 "    item_key VARCHAR(50) NOT NULL,\n" +
                 "    item_value VARCHAR(50) DEFAULT 'Unknown'\n" +
                 ")  WITH %CLASSPARAMETER DEFAULTGLOBAL = '^TORUS.ITEMS'";
 
             storage.iris.sql(sql)
                 .then(() => {
-                    console.log("Torus.Items created");
+                    console.log("TORUS.ITEMS created");
                 }).then(() => {
-                    storage.iris.chunk_insert('^TORUS.ITEMS.1', data).then((out) => {
+                    storage.iris.chunk_insert('TORUS.ITEMS', data).then((out) => {
                         console.log('inserted', out, Math.floor(+new Date() / 1000) - start_timer);
 
                     }).catch(err => { console.log(err); });
